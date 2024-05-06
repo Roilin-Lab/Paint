5264807ece11c25779ed818fe098d3624add8c44
skiri <skirills267@gmail.com>
2024-05-06 20:38:43 +0300

    Переработаны методы сохранения и загрузки, добавлен метод для получения всех холстов из рабочей папки

--- TestPaint/StorageManager.cs
+++ TestPaint/StorageManager.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using Windows.Storage.Search;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
 
@@ -14,16 +15,68 @@ namespace TestPaint
     {
         public static async void SaveCanvas(InkCanvas inkCanvas, CanvasDataObject canvasDataObject)
         {
-            StorageFolder storageFolder = await KnownFolders.GetFolderAsync(KnownFolderId.PicturesLibrary);
-            StorageFile file = await storageFolder.CreateFileAsync(canvasDataObject.FullName, CreationCollisionOption.ReplaceExisting);
+            StorageFolder storageFolder = await GetOrCreateWorkDirectory();
+            StorageFile file = await storageFolder.GetFileAsync(canvasDataObject.FullName);
             
+            WriteCanvaseInFile(inkCanvas, file);
+        }
+
+        public static async Task<CanvasDataObject> CreateCanvas()
+        {
+            StorageFolder storageFolder = await GetOrCreateWorkDirectory();
+            StorageFile file = await storageFolder.CreateFileAsync("Untitled.isf", CreationCollisionOption.GenerateUniqueName);
+
+            InkCanvas inkCanvas = new InkCanvas();
+            CanvasDataObject canvasDataObject = new CanvasDataObject(file.DisplayName, file.FileType);
+
+            WriteCanvaseInFile(inkCanvas, file);
+
+            return canvasDataObject;
+            
+        }
+
+        public static async void LoadCanvas(InkCanvas inkCanvas, CanvasDataObject canvasDataObject)
+        {
+            StorageFolder storageFolder = await GetOrCreateWorkDirectory();
+            StorageFile file = await storageFolder.GetFileAsync(canvasDataObject.FullName);
+
+            if (file != null)
+            {
+                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
+                using (var inputStream = stream.GetInputStreamAt(0))
+                {
+                    await inkCanvas.InkPresenter.StrokeContainer.LoadAsync(inputStream);
+                }
+                stream.Dispose();
+            }
+        }
+
+        public static async Task<IEnumerable<CanvasDataObject>> GetAllCanvasFromWorkDirectory()
+        {
+            StorageFolder workDirectory = await GetOrCreateWorkDirectory();
+
+            var queryOptions = new QueryOptions(CommonFolderQuery.DefaultQuery);
+            var query = workDirectory.CreateFileQueryWithOptions(queryOptions);
+            IEnumerable<StorageFile> fileList = await query.GetFilesAsync();
+
+            List<CanvasDataObject> canvasDataObjects = new List<CanvasDataObject>();
+            foreach (StorageFile file in fileList)
+            {
+                if (file.FileType == ".isf")
+                    canvasDataObjects.Add(new CanvasDataObject(file.DisplayName, file.FileType));
+            }
+            return canvasDataObjects;
+        }
+
+        private static async void WriteCanvaseInFile(InkCanvas inkCanvas, StorageFile file)
+        {
             if (file != null)
             {
                 CachedFileManager.DeferUpdates(file);
                 IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite);
                 using (IOutputStream outputStream = stream.GetOutputStreamAt(0))
                 {
-                    await inkCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream);
+                    await inkCanvas.InkPresenter.StrokeContainer.SaveAsync(outputStream, Windows.UI.Input.Inking.InkPersistenceFormat.Isf);
                     await outputStream.FlushAsync();
                 }
                 stream.Dispose();
@@ -39,34 +92,13 @@ namespace TestPaint
                     // File couldn't be saved.
                 }
             }
-            // User selects Cancel and picker returns null.
-            else
-            {
-                // Operation cancelled.
-            }
         }
 
-        public static async void LoadCanvas(InkCanvas inkCanvas, CanvasDataObject canvasDataObject)
+        private static async Task<StorageFolder> GetOrCreateWorkDirectory()
         {
-            StorageFolder storageFolder = await KnownFolders.GetFolderAsync(KnownFolderId.PicturesLibrary);
-            StorageFile file = await storageFolder.GetFileAsync(canvasDataObject.FullName);
-
-            
-
-            if (file != null)
-            {
-                IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read);
-                using (var inputStream = stream.GetInputStreamAt(0))
-                {
-                    await inkCanvas.InkPresenter.StrokeContainer.LoadAsync(inputStream);
-                }
-                stream.Dispose();
-            }
-            // User selects Cancel and picker returns null.
-            else
-            {
-                // Operation cancelled.
-            }
+            StorageFolder picturesLibrary = await KnownFolders.GetFolderAsync(KnownFolderId.PicturesLibrary);
+            StorageFolder workDirectory = await picturesLibrary.CreateFolderAsync("TestPaint", CreationCollisionOption.OpenIfExists);
+            return workDirectory;
         }
     }
 }
