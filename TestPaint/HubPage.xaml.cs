d59bc401b864369cd298b7571bd6e4fcb06dbf32
skiri <skirills267@gmail.com>
2024-05-06 20:54:55 +0300

    Добавлено создание нововго холста. Чтение существующих холстов из рабочей папки при навигации на страницу

--- TestPaint/HubPage.xaml.cs
+++ TestPaint/HubPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -18,22 +19,46 @@ namespace TestPaint
 {
     public sealed partial class HubPage : Page
     {
-        private IEnumerable<CanvasDataObject> CanvasData;
+        private ObservableCollection<CanvasDataObject> CanvasData;
 
         public HubPage()
         {
             this.InitializeComponent();
+            CanvasData = new ObservableCollection<CanvasDataObject>();
+        }
 
-            CanvasData = new List<CanvasDataObject>() 
-            { 
-                new CanvasDataObject("Untitled"), 
-                new CanvasDataObject("Hello World") 
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            IEnumerable<CanvasDataObject> canvasDataObjects = await StorageManager.GetAllCanvasFromWorkDirectory();
+
+            foreach (var item in canvasDataObjects)
+            {
+                CanvasData.Add(item);
             };
+            base.OnNavigatedTo(e);
         }
 
         private void listCanvas_ItemClick(object sender, ItemClickEventArgs e)
         {
             Frame.Navigate(typeof(DrawingPage), e.ClickedItem);
+        }
+
+        private async void newCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            CanvasDataObject newCanvas = await StorageManager.CreateCanvas();
+
+            CanvasData.Add(newCanvas);
+            Frame.Navigate(typeof(DrawingPage), newCanvas);
+        }
+
+        private void deleteCanvas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void renameCanvas_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
