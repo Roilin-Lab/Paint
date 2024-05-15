89cd212d7740e7ee1d475cd6db8bf8edf271ee52
skiri <skirills267@gmail.com>
2024-05-15 17:19:18 +0300

    Изменения UI/UX. Панорамирование холста. Клавиатурные скоращения для панели инструментов.

--- TestPaint/DrawingPage.xaml.cs
+++ TestPaint/DrawingPage.xaml.cs
@@ -10,10 +10,13 @@ using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Xml.Linq;
+using Windows.ApplicationModel.Resources;
 using Windows.Devices.Input;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -36,7 +39,7 @@ namespace TestPaint
         {
             this.InitializeComponent();
 
-            canvas.InkPresenter.InputDeviceTypes = Windows.UI.Core.CoreInputDeviceTypes.Mouse | Windows.UI.Core.CoreInputDeviceTypes.Pen;
+            canvas.InkPresenter.InputDeviceTypes =  CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Mouse;
         }
 
         private void DrawGridCanvas()
@@ -116,40 +119,38 @@ namespace TestPaint
             this.canvasControl = null;
         }
 
-        private void Page_Unloaded(object sender, RoutedEventArgs e)
+        private void PanBtn_Checked(object sender, RoutedEventArgs e)
         {
-            this.canvasControl.RemoveFromVisualTree();
-            this.canvasControl = null;
+            canvas.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+            canvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.None;
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.SizeAll, 1);
         }
 
-        private void canvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        private void PanBtn_Unchecked(object sender, RoutedEventArgs e)
         {
-            using(CanvasDrawingSession session = renderTarget.CreateDrawingSession())
+            canvas.ManipulationMode = ManipulationModes.None;
+            canvas.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Pen | CoreInputDeviceTypes.Mouse;
+            Window.Current.CoreWindow.PointerCursor = new CoreCursor(CoreCursorType.Arrow, 1);
+        }
+
+        private void canvas_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            var offsetY = frameCanvas.VerticalOffset;
+            var offsetX = frameCanvas.HorizontalOffset;
+
+            offsetY -= e.Delta.Translation.Y;
+            offsetX -= e.Delta.Translation.X;
+
+            frameCanvas.ChangeView(offsetX, offsetY, frameCanvas.ZoomFactor, true);
+        }
+
+        private void Page_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Space)
             {
-                session.DrawText("Hello World", 100, 100, Colors.Black);
+                PanBtn.IsChecked = !PanBtn.IsChecked;
+                e.Handled = true;
             }
-
-            args.DrawingSession.DrawImage(renderTarget);
-        }
-
-        private void canvasControl_CreateResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
-        {
-            renderTarget = new CanvasRenderTarget(canvasControl, canvasControl.Size);
-        }
-
-        private void SaveBtn_Click(object sender, RoutedEventArgs e)
-        {
-            var strokes = canvas.InkPresenter.StrokeContainer.GetStrokes().Select<InkStroke, StrokeData>((inkStroke, strokeData) => new StrokeData(inkStroke.GetInkPoints(), inkStroke.DrawingAttributes));
-            Layer layer = new Layer("test", true, 1, strokes);
-            CanvasDataObject.LayerManager.AddLayer(layer);
-            StorageManager.SaveToBSON(CanvasDataObject);
-        }
-
-        private async void LoadBtn_Click(object sender, RoutedEventArgs e)
-        {
-            CanvasDataObject canvasDataObject = await StorageManager.LoadFromBSON();
-
-            //canvas.InkPresenter.StrokeContainer.AddStrokes();
         }
     }
 }
